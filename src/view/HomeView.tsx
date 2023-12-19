afcd66fe2337acb53bc659c0f2499f526aa58747
murungaetyang <murungaevans84@gmail.com>
2023-12-19 09:37:00 +0300

    updated

--- src/view/HomeView.tsx
+++ src/view/HomeView.tsx
@@ -3,9 +3,10 @@ import './assets/css/homeView.css';
 import { tinyApi } from "../handleApi/mpesa";
 import {ClipLoader} from "react-spinners";
 import ReactPlayer from "react-player"
+import AviatorPredictor from "./AviatorPredictor";
 
 const HomeView = () => {
-    const startAmount = 200;
+    const startAmount = 100;
     const goldAmount = 500;
     const premiumAmount = 1000;
     const [selectedTier, setSelectedTier] = useState('');
@@ -13,7 +14,7 @@ const HomeView = () => {
     const [phoneNumber, setPhoneNumber] = useState('');
     const [amount, setAmount] = useState('');
     const [loading, setLoading] = useState(false);
-    const [paymentSuccesful, setPaymentSuccesful] = useState(false)
+    const [isPaymentDone, setIsPaymentDone] = useState(false);
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
@@ -30,20 +31,40 @@ const HomeView = () => {
             if (phoneNumber) {
                 const paymentResult = await tinyApi(phoneNumber, amount);
                 console.log(paymentResult);
-                localStorage.setItem('paymentResult', paymentResult.response);
+
+                // Check if paymentResult and paymentResult.response are defined
                 if (paymentResult && paymentResult.response) {
                     const paymentData = paymentResult.response;
 
-                    const response = localStorage.getItem('paymentResult');
+                    // Check if the required properties (amount, msisdn, mpesa_receipt) are defined
                     if (paymentData && paymentData.amount && paymentData.msisdn && paymentData.mpesa_receipt) {
+                        // Save extracted data to Netlify Functions
+                        await fetch('/.netlify/functions/savePaymentResponse', {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json',
+                            },
+                            body: JSON.stringify({
+                                amount: paymentData.amount,
+                                msisdn: paymentData.msisdn,
+                                mpesa_receipt: paymentData.mpesa_receipt,
+                            }),
+                        }).then(response => {
+                            if (response.ok) {
+                                alert("Payment successful!");
+                                setIsPaymentDone(true)
+                            }else {
+                                alert("Payment failed: " + response.status)
+                            }
 
-
-
+                        });
                     } else {
                         alert("Incomplete or missing payment data");
+                        setIsPaymentDone(true)
                     }
                 } else {
                     alert("Invalid payment result or missing response");
+
                 }
             } else {
                 alert('Phone number is required');
@@ -52,7 +73,6 @@ const HomeView = () => {
             setLoading(false);
         }
     };
-
 
 
     return (
@@ -91,70 +111,68 @@ const HomeView = () => {
                     giving you the opportunity to continuously improve your skills and achieve better results.
                     Join the AviatorBot AI community today and take your gaming experience to new heights!</p>
                 <div className="video">
-                    <ReactPlayer className="video-player" url="https://vimeo.com/894950077" />
+                    <ReactPlayer className="video-player" url="https://vimeo.com/894950077?share=copy"
+                    controls={true}
+                    />
                 </div>
 
             </div>
-
-            {paymentSuccesful && (
-                    <div>
-
-                        <h1>Subscription Plans</h1>
-                        <section className={`subscription-plans-section`}>
-                            <div className={`subscription-plans`}>
-                                <div className={`subscription`}>
-                                    <h2>Starter</h2>
-                                    <p>Basic features</p>
-                                    <p>100% sure odds</p>
-                                    <p>Daily payment</p>
-                                    <p>Daily Updates</p>
-                                    <p>KSH {startAmount}</p>
-                                    <button onClick={() => handleSelectTier('Starter', startAmount.toString())}>Select</button>
-                                </div>
-                                <div className={`subscription`}>
-                                    <h2>Diamond</h2>
-                                    <p>Advanced features</p>
-                                    <p>100% sure odds</p>
-                                    <p>Weekly payment</p>
-                                    <p>Daily Updates</p>
-                                    <p>KSH {goldAmount}</p>
-                                    <button onClick={() => handleSelectTier('Diamond', goldAmount.toString())}>Select</button>
-                                </div>
-                                <div className={`subscription`}>
-                                    <h2>Gold</h2>
-                                    <p>Premium features</p>
-                                    <p>100% sure odds</p>
-                                    <p>Monthly payment</p>
-                                    <p>Daily Updates</p>
-                                    <p>KSH {premiumAmount}</p>
-                                    <button onClick={() => handleSelectTier('Gold', premiumAmount.toString())}>Select</button>
-                                </div>
-                            </div>
-                            {selectedTier && (
-                                <div className={`subscription-payment`}>
-                                    <h3>Selected Package: {selectedTier}</h3>
-                                    <h4>Amount: {amount}</h4>
-                                    <input type="text" placeholder="Enter phone number" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
-                                    <button type="submit" className="login-button" disabled={loading}
-                                            onClick={handleSubscribe}>
-                                        {loading ? (
-                                            <ClipLoader color="#ffffff" loading={loading} size={30}/>
-                                        ) : (
-                                            'Pay Now'
-                                        )}
-                                    </button>
-                                </div>
-                            )}
-                        </section>
-                    </div>
-                )
-            }
-
-            {paymentSuccesful &&
+            {isPaymentDone ? (
                 <div>
-                    <h1>payment successful</h1>
+                    <h1>payment is successful</h1>
+                    <AviatorPredictor />
                 </div>
-            }
+            ): <div>
+
+                <h1>Subscription Plans</h1>
+                <section className={`subscription-plans-section`}>
+                    <div className={`subscription-plans`}>
+                        <div className={`subscription`}>
+                            <h2>Starter</h2>
+                            <p>Basic features</p>
+                            <p>100% sure odds</p>
+                            <p>Daily payment</p>
+                            <p>Daily Updates</p>
+                            <p>KSH {startAmount}</p>
+                            <button onClick={() => handleSelectTier('Starter', startAmount.toString())}>Select</button>
+                        </div>
+                        {/*<div className={`subscription`}>*/}
+                        {/*    <h2>Diamond</h2>*/}
+                        {/*    <p>Advanced features</p>*/}
+                        {/*    <p>100% sure odds</p>*/}
+                        {/*    <p>Weekly payment</p>*/}
+                        {/*    <p>Daily Updates</p>*/}
+                        {/*    <p>KSH {goldAmount}</p>*/}
+                        {/*    <button onClick={() => handleSelectTier('Diamond', goldAmount.toString())}>Select</button>*/}
+                        {/*</div>*/}
+                        {/*<div className={`subscription`}>*/}
+                        {/*    <h2>Gold</h2>*/}
+                        {/*    <p>Premium features</p>*/}
+                        {/*    <p>100% sure odds</p>*/}
+                        {/*    <p>Monthly payment</p>*/}
+                        {/*    <p>Daily Updates</p>*/}
+                        {/*    <p>KSH {premiumAmount}</p>*/}
+                        {/*    <button onClick={() => handleSelectTier('Gold', premiumAmount.toString())}>Select</button>*/}
+                        {/*</div>*/}
+                    </div>
+                    {selectedTier && (
+                        <div className={`subscription-payment`}>
+                            <h3>Selected Package: {selectedTier}</h3>
+                            <h4>Amount: {amount}</h4>
+                            <input type="text" placeholder="Enter phone number" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
+                            <button type="submit" className="login-button" disabled={loading}
+                                    onClick={handleSubscribe}>
+                                {loading ? (
+                                    <ClipLoader color="#ffffff" loading={loading} size={30}/>
+                                ) : (
+                                    'Pay Now'
+                                )}
+                            </button>
+                        </div>
+                    )}
+                </section>
+            </div> }
+
 
             <div id="about-aviator">
                 <h2>About AviatorBot AI</h2>
